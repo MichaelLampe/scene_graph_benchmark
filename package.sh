8136afdb6b523324a867f3369a947434b5ce3418
Michael Lampe <lampe@openai.com>
2022-09-15 10:39:31 -0700

    A bit more of the packaging dance

--- package.sh
+++ package.sh
@@ -1,6 +1,13 @@
+BASE_DIRECTORY=scene_graph_benchmark
+CURRENT_GIT_COMMIT=`git rev-list --max-count=1 HEAD`
+FILE_NAME=scene_graph_benchmark-michael-lampe-fork-$CURRENT_GIT_COMMIT-with-weights.tgz
+
+echo "Downloading relevant weights and label mappings"
 wget -nc https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/vinvl_vg_x152c4.pth
 wget -nc https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/VG-SGG-dicts-vgoi6-clipped.json
 
-CURRENT_GIT_COMMIT=`git rev-list --max-count=1 HEAD`
-echo $CURRENT_GIT_COMMIT
-tar -czvf scene_graph_benchmark-michael-lampe-fork-$CURRENT_GIT_COMMIT-with-weights.tgz .
+echo "Packaging commit $CURRENT_GIT_COMMIT"
+cp ..
+tar -czvf $FILE_NAME $BASE_DIRECTORY
+cp FILE_NAME $BASE_DIRECTORY
+cd $BASE_DIRECTORY
