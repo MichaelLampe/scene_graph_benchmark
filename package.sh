4cac5f6c921740fba28a90c4208b33e3cfdb7303
Michael Lampe <lampe@openai.com>
2022-09-15 10:28:59 -0700

    Re-use wget if file exists, pull in whole dir

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
-wget https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/vinvl_vg_x152c4.pth
-wget https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/VG-SGG-dicts-vgoi6-clipped.json
+wget -nc https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/vinvl_vg_x152c4.pth
+wget -nc https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_zoo/VG-SGG-dicts-vgoi6-clipped.json
 
 CURRENT_GIT_COMMIT=`git rev-list --max-count=1 HEAD`
 echo $CURRENT_GIT_COMMIT
-tar -czvf scene_graph_benchmark-michael-lampe-fork-$CURRENT_GIT_COMMIT-with-weights.tgz scene_graph_benchmark/
+tar -czvf scene_graph_benchmark-michael-lampe-fork-$CURRENT_GIT_COMMIT-with-weights.tgz .
