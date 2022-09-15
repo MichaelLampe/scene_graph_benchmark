b7c32b9f6060337d73f26df2de894cadc4fdf823
Michael Lampe <lampe@openai.com>
2022-09-15 10:42:28 -0700

    Dont repackage existing tars

--- package.sh
+++ package.sh
@@ -8,6 +8,7 @@ wget -nc https://penzhanwu2.blob.core.windows.net/sgg/sgg_benchmark/vinvl_model_
 
 echo "Packaging commit $CURRENT_GIT_COMMIT"
 cd ..
-tar -czvf $FILE_NAME $BASE_DIRECTORY
+# Exclude our own tar files
+tar --exclude="*.tgz" -czvf $FILE_NAME $BASE_DIRECTORY
 mv $FILE_NAME $BASE_DIRECTORY
 cd $BASE_DIRECTORY
